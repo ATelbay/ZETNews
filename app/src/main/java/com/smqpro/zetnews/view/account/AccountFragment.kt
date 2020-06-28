ed173efefe59f84a7f4e702bfb220ec7f482cd3f
smqpro@gmail.com <smqpro@gmail.com>
2020-06-28 19:58:30 +0600

    Auth

--- app/src/main/java/com/smqpro/zetnews/view/account/AccountFragment.kt
+++ app/src/main/java/com/smqpro/zetnews/view/account/AccountFragment.kt
@@ -1,53 +1,23 @@
 package com.smqpro.zetnews.view.account
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.smqpro.zetnews.R
-import com.smqpro.zetnews.util.TAG
 import com.smqpro.zetnews.util.load
 import kotlinx.android.synthetic.main.fragment_account.*
-import kotlinx.android.synthetic.main.fragment_account_signed_in.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 
 
-class AccountFragment : Fragment() {
+class AccountFragment : Fragment(R.layout.fragment_account) {
     private lateinit var fAuth: FirebaseAuth
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        fAuth = FirebaseAuth.getInstance()
-        Log.d(TAG, "onCreateView: user is ${fAuth.currentUser}")
-        return if (fAuth.currentUser != null)
-            inflater.inflate(R.layout.fragment_account_signed_in, container, false)
-        else
-            inflater.inflate(R.layout.fragment_account, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (fAuth.currentUser != null) {
-            initLogoutUser()
-        } else {
-            initSignUpUser()
-            initLoginUser()
-        }
-
-
+        fAuth = FirebaseAuth.getInstance()
+        initAuthUser()
     }
 
     override fun onStart() {
@@ -60,14 +30,21 @@ class AccountFragment : Fragment() {
             signed_in_login_tv.text = it.displayName
             signed_in_mail_tv.text = it.email
             signed_in_picture_iv.load(it.photoUrl.toString())
+            signed_in_appbar.visibility = View.VISIBLE
+            signed_in_auth_button.text = getString(R.string.log_out)
         }
     }
 
-    private fun initLogoutUser() {
-        signed_in_logout_button.setOnClickListener {
-            fAuth.signOut()
-            reloadFragment()
+    private fun initAuthUser() {
+        signed_in_auth_button.setOnClickListener {
+            if (fAuth.currentUser != null) {
+                fAuth.signOut()
+                reloadFragment()
+            } else {
+                findNavController().navigate(AccountFragmentDirections.toAuthFragment())
+            }
         }
+
     }
 
     private fun initSwitchTheme() {
@@ -76,81 +53,9 @@ class AccountFragment : Fragment() {
         }
     }
 
-    private fun initLoginUser() {
-        account_login_button.setOnClickListener {
-            checkInsertedData {
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        fAuth.signInWithEmailAndPassword(
-                            account_email_til_et.text.toString(),
-                            account_password_til_et.text.toString()
-                        ).await()
-                        withContext(Dispatchers.Main) {
-                            reloadFragment()
-                        }
-                    } catch (e: Exception) {
-                        Log.d(TAG, "Auth failed", e)
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(
-                                context, "Something went wrong. Please, try again later",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
-    private fun initSignUpUser() {
-        account_sign_up_button.setOnClickListener {
-            checkInsertedData {
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        fAuth.createUserWithEmailAndPassword(
-                            account_email_til_et.text.toString(),
-                            account_password_til_et.text.toString()
-                        ).await()
-                        withContext(Dispatchers.Main) {
-                            findNavController().navigate(AccountFragmentDirections.reloadFragment())
-                        }
-                    } catch (e: Exception) {
-                        Log.d(TAG, "Auth failed", e)
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(
-                                context, "Something went wrong. Please, try again later",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private fun checkInsertedData(
-        block: () -> Unit
-    ) {
-        if (
-            account_email_til_et.text.toString().isNotEmpty() &&
-            account_password_til_et.text.toString().isNotEmpty()
-        ) {
-            account_login_til.error = null
-            account_password_til.error = null
-            block()
-        } else {
-            if (account_email_til_et.text.toString().isEmpty()) {
-                account_login_til.error = "Login can not be empty"
-            }
-            if (account_password_til_et.text.toString().isEmpty()) {
-                account_password_til.error = "Password can not be empty"
-            }
-        }
-    }
 
     private fun reloadFragment() {
-        findNavController().navigate(AccountFragmentDirections.reloadFragment())
+        findNavController().navigate(AccountFragmentDirections.reloadAccountFragment())
     }
 
 }
---
+++ app/src/main/java/com/smqpro/zetnews/view/account/AuthFragment.kt
@@ -0,0 +1,105 @@
+package com.smqpro.zetnews.view.account
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.smqpro.zetnews.R
+import com.smqpro.zetnews.util.TAG
+import kotlinx.android.synthetic.main.fragment_auth.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class AuthFragment : Fragment(R.layout.fragment_auth) {
+    private lateinit var fAuth: FirebaseAuth
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fAuth = FirebaseAuth.getInstance()
+        initLoginUser()
+        initSignUpUser()
+    }
+
+    private fun initLoginUser() {
+        account_login_button.setOnClickListener {
+            checkInsertedData {
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        fAuth.signInWithEmailAndPassword(
+                            account_email_til_et.text.toString(),
+                            account_password_til_et.text.toString()
+                        ).await()
+                        withContext(Dispatchers.Main) {
+                            findNavController().popBackStack()
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "Auth failed", e)
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(
+                                context, "Something went wrong. Please, try again later",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun initSignUpUser() {
+        account_sign_up_button.setOnClickListener {
+            checkInsertedData {
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        fAuth.createUserWithEmailAndPassword(
+                            account_email_til_et.text.toString(),
+                            account_password_til_et.text.toString()
+                        ).await()
+                        withContext(Dispatchers.Main) {
+                            findNavController().navigate(AccountFragmentDirections.reloadAccountFragment())
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "Auth failed", e)
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(
+                                context, "Something went wrong. Please, try again later",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkInsertedData(
+        block: () -> Unit
+    ) {
+        if (
+            account_email_til_et.text.toString().isNotEmpty() &&
+            account_password_til_et.text.toString().length > 5
+        ) {
+            account_login_til.error = null
+            account_password_til.error = null
+            block()
+        } else {
+            if (account_email_til_et.text.toString().isEmpty()) {
+                account_login_til.error = "Login can not be empty"
+            }
+            if (account_password_til_et.text.toString().isEmpty()) {
+                account_password_til.error = "Password can not be empty"
+            }
+            if (account_password_til_et.text.toString().isNotEmpty() &&
+                account_password_til_et.text.toString().length < 6) {
+                account_password_til.error = "Password must contain more than 6 characters"
+            }
+        }
+    }
+}
